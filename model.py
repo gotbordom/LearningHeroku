df833f0d6cb473667fddddad360a7cd9136ad5c4
GotB <gotbordom@gmail.com>
2021-05-15 21:36:27 -0600

    Testing just html now

--- model.py
+++ model.py
@@ -51,37 +51,37 @@ app = Flask(__name__)
 
 #import tabulate
 #table = tabulate.table(g)
-data = pd.read_csv("https://github.com/Nick-Milliken/deploy-ml/raw/main/ramen/ramen-ratings.csv")
 
-X = data.Stars
+# data = pd.read_csv("https://github.com/Nick-Milliken/deploy-ml/raw/main/ramen/ramen-ratings.csv")
 
-X = X.replace('Unrated', np.NaN)
-X = pd.to_numeric(X)
-X = np.nan_to_num(X)
-X = X.reshape(-1, 1)
+# X = data.Stars
 
-y = data.Country
+# X = X.replace('Unrated', np.NaN)
+# X = pd.to_numeric(X)
+# X = np.nan_to_num(X)
+# X = X.reshape(-1, 1)
 
-X, y = X, y
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    random_state=0)
-pipe = Pipeline([('scaler', StandardScaler()), ('lr', LogisticRegression())])
-# The pipeline can be used as any other estimator
-# and avoids leaking the test set into the train set
-pipe.fit(X_train, y_train)
-Pipeline(steps=[('scaler', StandardScaler()), ('lr', LogisticRegression())])
-pipe.score(X_test, y_test)
+# y = data.Country
 
-lr = LogisticRegression(random_state = 42)
-lr.fit = lr.fit(X_train, y_train)
-lr.fit = lr.predict(X)
-lr.proba = lr.predict_proba(X)
+# X, y = X, y
+# X_train, X_test, y_train, y_test = train_test_split(X, y,
+#                                                     random_state=0)
+# pipe = Pipeline([('scaler', StandardScaler()), ('lr', LogisticRegression())])
+# # The pipeline can be used as any other estimator
+# # and avoids leaking the test set into the train set
+# pipe.fit(X_train, y_train)
+# Pipeline(steps=[('scaler', StandardScaler()), ('lr', LogisticRegression())])
+# pipe.score(X_test, y_test)
+
+# lr = LogisticRegression(random_state = 42)
+# lr.fit = lr.fit(X_train, y_train)
+# lr.fit = lr.predict(X)
+# lr.proba = lr.predict_proba(X)
 #print(lr.fit)
 #print(lr.proba)
 
 
 
-@app.route("/")
 @app.route("/welcome")
 def welcome():
     return render_template("index.html")
